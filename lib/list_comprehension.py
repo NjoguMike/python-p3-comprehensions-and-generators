0b28b3a50419adaf80cefe3c03627abafd840108
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-09-29 12:35:24 +0300

    Complete Comprehensions

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    new_list = [num for num in num_list if num % 2 != 1]
+    return new_list
 
 def make_exclamation(sentence_list):
-    pass
+    new_str = [n_str+'!' for n_str in sentence_list]
+    return new_str
